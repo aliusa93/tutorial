8f7dbc5b47dca416df8126b102a4bd9ceff391ae
aliusa93 <84295361+aliusa93@users.noreply.github.com>
2021-05-30 13:18:29 -0400

    Video 1

---
+++ #1 Setup/index.js
@@ -0,0 +1,53 @@
+const Discord = require('discord.js')
+
+const client = new Discord.Client()
+
+const config = require('./config.json')
+
+
+
+
+
+client.on('ready', () => {
+    console.log('Your bot is online!')
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+client.login(config.token)
